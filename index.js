46e371633e0d80d604ee514ad5b7396e3fcc8f24
Mehefuj Ali <mehefujalim@gmail.com>
2025-01-03 00:13:06 +0530

    data add to cart created

--- index.js
+++ index.js
@@ -1,50 +1,46 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
-require('dotenv').config()
+require("dotenv").config();
 const port = process.env.PORT || 8080;
 //Middle ware
 app.use(cors());
 app.use(express.json());
 
-
-
-
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.negmw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 const client = new MongoClient(uri, {
-      serverApi: {
-        version: ServerApiVersion.v1,
-        strict: true,
-        deprecationErrors: true,
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    const menuCollection = client.db("bistro-boss").collection("menu");
+    const cartCollection = client.db("bistro-boss").collection("cart");
+
+    app.get("/menu", async (req, res) => {
+      const menu = await menuCollection.find().toArray();
+      res.send(menu);
+    });
+    app.post("/cart", async (req, res) => {
+      try {
+        const result = await cartCollection.insertOne(req.body);
+        res.send(result);
+      } catch {
+        res.send({ status: false });
       }
     });
 
-
-    async function run() {
-      try {
-        const menuCollection = client.db('bistro-boss').collection('menu')
-          
-        app.get('/menu' ,async (req,res)=>{
-            const menu = await menuCollection.find().toArray()
-            res.send(menu)
-        })
-         
-        console.log("You successfully connected to MongoDB!");
-      } finally {
-        
-      }
-    }
-    run().catch(console.dir);
-
-
-
-
-
-
-
-
+    console.log("You successfully connected to MongoDB!");
+  } finally {
+  }
+}
+run().catch(console.dir);
 
 app.get("/", (req, res) => {
   try {
@@ -58,6 +54,6 @@ app.get("/", (req, res) => {
   }
 });
 
-app.listen(port, ()=>{
-      console.log("SURVER IN RUNNING ON PORT :" , port)
-})
+app.listen(port, () => {
+  console.log("SURVER IN RUNNING ON PORT :", port);
+});
