700fdaa6eaaeea5c7ce39427e2c272557a14895a
Mehefuj Ali <mehefujalim@gmail.com>
2025-01-06 23:16:06 +0530

    user role update and delete user api created

--- index.js
+++ index.js
@@ -37,10 +37,29 @@ async function run() {
       const result = await userCollection.insertOne(user);
       res.send(result);
     });
-    app.get('/users' , async(req,res)=>{
-      const users = await userCollection.find().toArray() 
-      res.send(users)
-    })
+    app.get("/users", async (req, res) => {
+      const users = await userCollection.find().toArray();
+      res.send(users);
+    });
+    app.delete("/users/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await userCollection.deleteOne(query);
+      res.send(result);
+    });
+    app.patch("/user/admin/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const updateDoc = {
+        $set: {
+          role: "admin",
+        },
+      };
+      const result = await userCollection.updateOne(filter, updateDoc);
+      res.send(result);
+    });
+
+    // food releted
 
     app.get("/menu", async (req, res) => {
       const menu = await menuCollection.find().toArray();
